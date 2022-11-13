7dbc3f56d74637e6569691b26a01d0292ffb8522
Gerrritvt <gerrit1994@hotmail.com>
2022-11-13 19:45:00 +0100

    update grading message

--- src/everyday_mail.py
+++ src/everyday_mail.py
@@ -1,6 +1,6 @@
 import lichess.api
 from datetime import datetime
-from functions import progress_message, yesterday_message, send_simple_message
+from functions import progress_message, yesterday_message, send_simple_message, grades_history
 from credentials import username, mail_api_url, key, sender, to
 
 
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     # API calls
     user = lichess.api.user(username)
     games = lichess.api.user_games(username, max=100, perfType='blitz', analysed=True, opening=True, evals=True)
+    games_2 = lichess.api.user_games(username, max=200, perfType='blitz', analysed=True, opening=True, evals=True)
 
     # Progress text
     progress_string = progress_message(user)
@@ -15,9 +16,13 @@ if __name__ == "__main__":
     # Yesterday update
     yesterday_string = yesterday_message(games, username)
     
+    # Recent games update
+    recent_games_string = grades_history(games_2, username, min_played=5)
+    print(recent_games_string)
+
     # Send email
     date = datetime.now().strftime("%d-%m-%Y")
     subject = f"Lichess update {date}"
-    text = f"{progress_string}\n\n{yesterday_string}"
+    text = f"{progress_string}\n\n{yesterday_string}\n\n{recent_games_string}"
     send_simple_message(mail_api_url, key, sender, to, subject, text)
     print("Email succesful")
